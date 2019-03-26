a58defc91bd199302413a130da2708efc048f6d1
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-03-26 16:43:14 +0100

    import real android project using kotlin gradle dsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,26 @@
-plugins {
-    application
-    kotlin("jvm") version "1.3.20"
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.1.3")
+        classpath(kotlin("gradle-plugin", version = "1.3.20"))
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle.kts files
+    }
 }
 
-application {
-    mainClassName = "samples.HelloWorldKt"
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
 }
 
-dependencies {
-    compile(kotlin("stdlib"))
-}
-
-repositories {
-    jcenter()
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
