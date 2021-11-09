0274f834e88fbda76de87db3951350dccf336a25
Olivér Falvai <oliver.falvai@bitrise.io>
2021-11-09 09:07:38 +0100

    Upgrade Gradle, AGP, Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.config.KotlinCompilerVersion
 plugins {
     id("com.android.application")
     kotlin("android")
-    kotlin("android.extensions")
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.1.3")
-        classpath(kotlin("gradle-plugin", version = "1.3.20"))
+        classpath("com.android.tools.build:gradle:7.0.3")
+        classpath(kotlin("gradle-plugin", version = "1.5.31"))
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
